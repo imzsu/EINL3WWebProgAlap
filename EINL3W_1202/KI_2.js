62d34a648dcc5716322c81e4cad30c0937f1e7d8
imzsu <kuknyoi@gmail.com>
2025-12-04 09:09:13 +0100

    21.Pract

---
+++ EINL3W_1202/KI_2.js
@@ -0,0 +1,42 @@
+$(document).ready(function ()
+{
+    $("#load_data").click(function ()
+    {
+        $("#area").html("");
+
+        $("#area").append("<h2>TOKAJ HEGYALJA EGYETEM</h2>");
+
+        $.getJSON("EINL3W_2.json", function (data)
+        {
+            $("#area").append("<b>Cím:</b>");
+            $("#area").append("&nbsp;" + data.cím["iranyitoszam"]);
+            $("#area").append("&nbsp;" + data.cím["varos"]);
+            $("#area").append("&nbsp;" + data.cím["utca"] + "<br><br>");
+
+            $("#area").append("<b>Telefonszám:</b><br><ul>");
+
+            for (let i = 0; i < data["telefonszam"].length; i ++ )
+            {
+                $("#area").append("<li>" + data["telefonszam"][i].tipus + ":&nbsp;" + data["telefonszam"][i].szam + "</li>");
+            }
+
+            $("#area").append("</ul><br><b>THE, PTI Órarend 2025 ősz</b><br></br>");
+
+            for (let i = 0; i < data.ora.length; i ++ )
+            {
+                $("#area").append("<b>Tárgy:</b>&nbsp;" + data.ora[i].targy +"<br><br>");
+
+                $("#area").append("<b>Időpont:</b><br>&nbsp;Nap:&nbsp;" + data.ora[i].idopont.nap + "<br>&nbsp;Tól:&nbsp;" + data.ora[i].idopont.tol + "<br>&nbsp;Ig:&nbsp;" + data.ora[i].idopont.ig + "<br><br>");
+
+                $("#area").append("<b>Helyszín:</b>&nbsp;" + data.ora[i].helyszin + "<br><br>");
+
+                $("#area").append("<b>Oktató:</b>&nbsp;" + data.ora[i].oktato + "<br><br>");
+
+                $("#area").append("<b>Szak:</b>&nbsp;" + data.ora[i].szak + "<br><br>");
+
+                $("#area").append("<b>Típus:</b>&nbsp;" + data.ora[i].tipus + "<br><br>");
+                $("#area").append("<hr>");
+            } 
+        });
+    });
+});
